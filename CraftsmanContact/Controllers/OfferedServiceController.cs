a94bdbf4c5fe655f358ec2423c28a471e5fe5c18
EviBera <beraevi@gmail.com>
2024-02-28 14:15:03 +0100

    Add controller for offered services

---
+++ CraftsmanContact/Controllers/OfferedServiceController.cs
@@ -0,0 +1,36 @@
+using CraftsmanContact.Models;
+using CraftsmanContact.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CraftsmanContact.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OfferedServiceController : ControllerBase
+{
+    private readonly ILogger<OfferedServiceController> _logger;
+    private readonly IOfferedServiceRepository _offeredServiceRepository;
+
+    public OfferedServiceController(ILogger<OfferedServiceController> logger, IOfferedServiceRepository osr)
+    {
+        _logger = logger;
+        _offeredServiceRepository = osr;
+    }
+    
+    // GET
+    [HttpGet]
+    public async Task<ActionResult<List<OfferedService>>> GetAllAsync()
+    {
+        try
+        {
+            var offeredServices = _offeredServiceRepository.GetAll();
+            return Ok(offeredServices);
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error getting offered services.");
+            return NotFound($"Error getting offered services, {e.Message}");
+        }
+    }
+}
