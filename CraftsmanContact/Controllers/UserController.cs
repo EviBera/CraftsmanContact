7ac4cf802102233b2fabcee2442dc7a1dcede02a
EviBera <beraevi@gmail.com>
2024-03-05 22:09:24 +0100

    Add functionality to register a user with extended properties

---
+++ CraftsmanContact/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+using CraftsmanContact.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CraftsmanContact.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+
+    public UserController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterAsync(RegisterModel model)
+    {
+        try
+        {
+            var user = new AppUser()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                PhoneNumber = model.PhoneNumber,
+                UserName = model.Email,
+                PasswordHash = model.Password
+            };
+            var result = await _userManager.CreateAsync(user, user.PasswordHash);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result);
+                
+            }
+
+            return Ok("Successful registration");
+
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, $"Registration failed, {e.Message}");
+        }
+    }
+
+}
---
+++ CraftsmanContact/Models/RegisterModel.cs
@@ -0,0 +1,10 @@
+namespace CraftsmanContact.Models;
+
+public class RegisterModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+}
