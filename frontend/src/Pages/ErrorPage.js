68eba03020759e151dd474c48e4e2cef8ad40abc
EviBera <beraevi@gmail.com>
2024-04-04 16:51:43 +0200

    Add basic error page

--- frontend/src/Pages/ErrorPage.js
+++ frontend/src/Pages/ErrorPage.js
@@ -1,6 +1,18 @@
+import { useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
+
+    const error = useRouteError();
+    console.error(error);
+
     return (
-        <h1>Error</h1>
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+            <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
     )
 
 }
