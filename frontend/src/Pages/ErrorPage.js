90b0736bb9e0a678dacb0653edb904cc0c577004
EviBera <beraevi@gmail.com>
2024-04-09 16:56:26 +0200

    Play with Bootstrap on Error page

--- frontend/src/Pages/ErrorPage.js
+++ frontend/src/Pages/ErrorPage.js
@@ -1,20 +1,31 @@
 import { useRouteError } from "react-router-dom";
 import './App.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const ErrorPage = () => {
 
-    const error = useRouteError();
-    console.error(error);
+  const error = useRouteError();
+  console.error(error);
 
-    return (
-      <div className="error-page">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
+  return (
+    <Container className="error-page">
+      <Row>
+        <Col>
+          <h1>Oops!</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <p>
             <i>{error.statusText || error.message}</i>
-        </p>
-      </div>
-    )
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  )
 
 }
 
