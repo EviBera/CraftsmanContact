4af127622598df0d46182ca4b5999414b74641d0
EviBera <beraevi@gmail.com>
2024-04-10 16:50:19 +0200

    Add context hook for handling selected service

--- frontend/src/Pages/App.js
+++ frontend/src/Pages/App.js
@@ -1,10 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import IDContext from './IDContext';
+import { useState } from 'react';
 import logo from '../logo.svg';
 import './App.css';
 //import { Outlet } from 'react-router-dom';
 import OfferedServiceList from './OfferedServiceList';
+import CraftsmenByServiceList from './CraftsmenByServiceList'
 
 function App() {
+
+  const [selectedId, setSelectedId] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,10 +19,11 @@ function App() {
         </p>
       </header>
       <body>
-        <OfferedServiceList />
+        <IDContext.Provider value={{ selectedId, setSelectedId }}>
+          <OfferedServiceList />
+          {selectedId && <CraftsmenByServiceList />}
+        </IDContext.Provider>
       </body>
-        
-      
     </div>
   );
 }
