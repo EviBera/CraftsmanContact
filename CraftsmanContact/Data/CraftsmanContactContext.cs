a11f9f1d4a332c2306d432fb0b09e5e84519ebcd
EviBera <beraevi@gmail.com>
2024-04-15 16:39:51 +0200

    Extend DB with roles

--- CraftsmanContact/Data/CraftsmanContactContext.cs
+++ CraftsmanContact/Data/CraftsmanContactContext.cs
@@ -1,4 +1,5 @@
 using CraftsmanContact.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,21 @@ public class CraftsmanContactContext : IdentityDbContext<AppUser>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        List<IdentityRole> roles = new List<IdentityRole>
+        {
+            new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "USER"
+            }
+        };
+        modelBuilder.Entity<IdentityRole>().HasData(roles);
         
         modelBuilder.Entity<AppUser>().ToTable("AppUsers");
         
