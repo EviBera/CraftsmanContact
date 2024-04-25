7261859ddf3625f8a3fb0c22f7cf845a0d2ddf0c
EviBera <beraevi@gmail.com>
2024-04-25 16:48:34 +0200

    Add message and redirection to Contact card after action

--- frontend/src/Pages/Contact.js
+++ frontend/src/Pages/Contact.js
@@ -1,32 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import ContactForm from "../Components/ContactForm";
-
-
-const fetchDeal = async (request, token) => {
-
-    const url = "http://localhost:5213/api/deal";
-
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        },
-        body: JSON.stringify(request),
-    });
-    console.log(response);
-    if (response.ok) {
-        console.log("Success");
-    } else {
-        console.log("Failed");
-        console.error(response.text());
-    }
-}
+import NavigationBar from './NavigationBar';
+import { Container } from 'react-bootstrap';
+import Loading from "../Components/Loading";
+import './App.css';
 
 const Contact = () => {
 
     const [submitted, setSubmitted] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [message, setMessage] = useState("");
+
     const navigate = useNavigate();
 
     const craftsmanId = localStorage.getItem('craftsmanId');
@@ -35,6 +20,7 @@ const Contact = () => {
     const client = JSON.parse(clientString);
     //console.log(craftsmanId + " .. " + serviceId);
 
+    const url = "http://localhost:5213/api/deal";
     const request = {
         "craftsmanId": craftsmanId,
         "clientId": client.id,
@@ -43,17 +29,55 @@ const Contact = () => {
 
     useEffect(() => {
         if (submitted) {
+
             console.log("Going to fetch...");
             console.log(request);
-            fetchDeal(request, client.token);
-            setTimeout(() => navigate('/'), 1000);
+
+            setLoading(true);
+
+            const fetchDeal = async () => {
+                try {
+                    const response = await fetch(url, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + client.token
+                        },
+                        body: JSON.stringify(request),
+                    });
+
+                    console.log(response);
+
+                    if (response.ok) {
+                        setMessage("We've sent your demand to the craftsman.");
+                    } else {
+                        setMessage("Unsuccessful demand. Try again later.");
+                    }
+
+                } catch (error) {
+                    console.error('Error:', error);
+                }
+                setLoading(false);
+            };
+            fetchDeal();
+
+            setTimeout(() => navigate('/'), 1500);
         }
     }, [submitted]);
 
+    if (loading) {
+        return < Loading />
+    };
+
     return (
         <>
-        <ContactForm setSubmitted={setSubmitted} />
-        
+            <NavigationBar />
+            {!message && <ContactForm setSubmitted={setSubmitted} />}
+            {message &&
+                <Container className="contact-message">
+                    {message}
+                </Container>}
+
         </>
     )
 }
