52151312a0a9657e7bdee9ddb1a136ee3d6852a5
EviBera <beraevi@gmail.com>
2024-04-30 14:32:02 +0200

    Use plain table, convert date for display

--- frontend/src/Components/DealTable/DealTable.jsx
+++ frontend/src/Components/DealTable/DealTable.jsx
@@ -1,53 +1,51 @@
 import { Container } from 'react-bootstrap';
-import Table from 'react-bootstrap/Table';
 import "./DealTable.css";
 
 const DealTable = (props) => {
 
-    console.log(props);
     const deals = props.deals;
-    console.log(deals);
 
-    const ConvertBoolToString = (bool) => {
-        if(bool) 
-            return 'Yes'
-        else
-         return 'Not yet.'
+    const DateConverter = (input) => {
+        let date = new Date(input);
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        let day = date.getDate();
+
+        return day + "/" + month + "/" + year;
     }
 
     return (
-        /*         <>
-        {deals && deals.map((deal) => (<p key={deal.dealId}>{deal.dealId}</p>))}
-        </>
-        */
-        <>
-            <Container className='title'>
-                <h1>My deals</h1>
-            </Container>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>Craftsman</th>
-                        <th>Service</th>
-                        <th>Datum of request</th>
-                        <th>Is it accepted by the craftsman?</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {deals && deals.map((deal) => (
-                        <tr key={deal.dealId}>
-                            <td></td>
-                            <td>{deal.craftsmanId}</td>
-                            <td>{deal.offeredServiceId}</td>
-                            <td>{deal.createdAt}</td>
-                            <td>{ConvertBoolToString(deal.isAcceptedByCraftsman)}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
+        <table>
+            <thead>
+                <tr >
+                    <th colspan="6" className='title'>
+                        My deals
+                    </th>
+                </tr>
 
-        </>)
+                <tr>
+                    <th>No.</th>
+                    <th>Deal Id</th>
+                    <th>Craftsman</th>
+                    <th>Service</th>
+                    <th>Date of my request (d/m/yyyy)</th>
+                    <th>Is it accepted by the craftsman?</th>
+                </tr>
+            </thead>
+            <tbody>
+                {deals && deals.map((deal) => (
+                    <tr key={deal.dealId}>
+                        <td>{deals.indexOf(deal) + 1}</td>
+                        <td>{deal.dealId}</td>
+                        <td>{deal.craftsmanId}</td>
+                        <td>{deal.offeredServiceId}</td>
+                        <td>{DateConverter(deal.createdAt)}</td>
+                        <td>{deal.isAcceptedByCraftsman ? 'Yessss!' : 'Not yet.'}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
 }
 
 export default DealTable;
