8f642c530aa8effd1dddfbfafba33d177e2474ab
EviBera <beraevi@gmail.com>
2024-05-17 21:15:58 +0200

    Add fetches to register/remove user's services

--- frontend/src/Pages/ServiceHandler.js
+++ frontend/src/Pages/ServiceHandler.js
@@ -8,7 +8,7 @@ const ServiceHandler = () => {
     const storedLoggedInUserString = localStorage.getItem('loggedInUser');
     const storedLoggedInUser = JSON.parse(storedLoggedInUserString);
     const headers = { 'Authorization': 'Bearer ' + storedLoggedInUser.token };
-    
+
     const [servicesOfUser, setServicesOfUser] = useState(null);
     const [removables, setRemovables] = useState(null);
     const [recordables, setRecordables] = useState(null);
@@ -17,23 +17,77 @@ const ServiceHandler = () => {
 
     useEffect(() => {
 
-      fetch(URLS.user.servicesByUser(storedLoggedInUser.id), { headers })
-        .then(response => response.json())
-        .then(data => {
-            setServicesOfUser(data)})
-    }, []);
+        fetch(URLS.user.servicesByUser(storedLoggedInUser.id), { headers })
+            .then(response => response.json())
+            .then(data => {
+                setServicesOfUser(data)
+            })
+    }, [updateTrigger]);
+
+    //console.log(removables);
     
-    console.log(removables);
-    console.log(recordables);
+    useEffect(() => {
+        
+        if (recordables) {
+            
+            console.log(recordables);
+            recordables.map((item) => {
+                fetch(URLS.user.registerService(storedLoggedInUser.id, item), {
+                    method: 'PATCH',
+                    headers,
+                    body: JSON.stringify({
+                        "userId": storedLoggedInUser.id,
+                        "serviceId": item
+                    }),
+                })
+                    .then(res => res.text())
+                    .then((data) => {
+                        console.log(data);
+                        triggerUpdate()})
+                    .catch(error => {
+                        console.error('Failed to fetch data:', error);
+                    });
+            })
+        }
+
+    }, [recordables])
+
+
+    useEffect(() => {
+        
+        if (removables) {
+            
+            console.log(removables);
+            removables.map((item) => {
+                fetch(URLS.user.removeService(storedLoggedInUser.id, item), {
+                    method: 'PATCH',
+                    headers,
+                    body: JSON.stringify({
+                        "userId": storedLoggedInUser.id,
+                        "serviceId": item
+                    }),
+                })
+                    .then(res => res.text())
+                    .then((data) => {
+                        console.log(data);
+                        triggerUpdate()})
+                    .catch(error => {
+                        console.error('Failed to fetch data:', error);
+                    });
+            })
+        }
+
+    }, [removables])
+
 
     const triggerUpdate = () => {
         setUpdateTrigger(current => current + 1);
     }
 
-    return(
+    return (
         <>
-        <NavigationBar />
-        <ServiceHandlerTable props = { { servicesOfUser, setRemovables, setRecordables, triggerUpdate } } />
+            <NavigationBar />
+            <ServiceHandlerTable props={{ servicesOfUser, setRemovables, setRecordables }} />
         </>
     )
 }
